1b34f7f179117c3e50926a85b8e27ab34b2f7c82
Tabithaokoth <kobbitho@gmail.com>
2024-03-28 12:16:56 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your code here!
+// const element = document.createElement("div");
+// document.body.append(element);
+
+// const ul = document.createElement("ul");
+
+// for (let i = 0; i < 3; i++) {
+//     const li = document.createElement ("li");
+//     li.textContent = (i + 1).toString(); // turn it into a string
+//     ul.append(li);
+   
+    // element.append(ul);
+
+
+    
+    // element.textContent = "You've changed what's on the screen!";
+
+    // element.style.fontSize ="24px";
+    // element.style.marginLeft ="30px";
+    // element.style.lineHeight = 2;
+    // element.style.color = "blue";
+
+    // element.className = "pet-listing dog";
+    
+    // document.querySelector(main);
+
+    
+// }
+main.remove()
+
+const newHeader = document.createElement("h1")
+
+newHeader.id =( "victory")
+
+newHeader.innerHTML = "Eli is the champion"
